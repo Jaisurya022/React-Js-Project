8d1e2ee4dcef1ea398a59560da8b23ee2d293e5b
Jaisurya022 <143484533+Jaisurya022@users.noreply.github.com>
2023-09-27 09:10:04 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
+import { Navbar } from './components/navbar'
+import {Shop} from './pages/shop/shop'
+import {Cart} from './pages/cart/cart'
+import { ShopContextProvider } from './context/shop-context';
+
+
+function App() {
+
+       return(
+         
+       
+            <>
+                 <ShopContextProvider>
+               <Router>
+
+                <Navbar />
+
+                  <Routes>
+                    <Route path="/" element={ <Shop />} />
+                    <Route path="/cart" element={<Cart />} />
+
+                  </Routes>
+
+               </Router>
+
+               </ShopContextProvider>
+            
+               </>
+
+
+
+
+       );     
+    
+ 
+}
+
+export default App
---
+++ src/products.js
@@ -0,0 +1,86 @@
+import product1 from "./assets/1.png";
+import product2 from "./assets/2.png";
+import product3 from "./assets/3.png";
+import product4 from "./assets/4.png";
+import product5 from "./assets/5.png";
+import product6 from "./assets/6.webp";
+import product7 from "./assets/7.webp";
+import product8 from "./assets/8.webp";
+import product9 from "./assets/9.jpeg";
+
+
+export const PRODUCTS = [
+
+    {
+        id:1,
+        productName: "IPhone 15 ",
+        price: 19000,
+        productImage: product1,
+
+    },
+
+
+    {
+        id:2,
+        productName: "Macbook Pro 2023 (M1) ",
+        price: 250000,
+        productImage: product2,
+
+    },
+
+
+    {
+        id:3,
+        productName: " Cannon M50 Camera ",
+        price: 150000,
+        productImage: product3,
+
+    },
+
+
+    {
+        id:4,
+        productName: " WLS Van Gogh Denim Jacket ",
+        price: 1900,
+        productImage: product4,
+
+    },
+    {
+        id:5,
+        productName: " LED Light Strips",
+        price: 15000,
+        productImage: product5,
+
+    },
+    {
+        id:6,
+        productName: "SPECTRUM LS TEE ",
+        price: 2000,
+        productImage: product6,
+
+    },
+    {
+        id:7,
+        productName: " AUTO SERVICE SHIRT by GOLF WANG ",
+        price: 1500,
+        productImage: product7,
+
+    },
+    {
+        id:8,
+        productName: " DONT'T TRIP UNSTRUCTURED HAT ",
+        price: 1000,
+        productImage: product8,
+
+    },
+    {
+        id:9,
+        productName: " BLACK CORDLESS HEADPHONE",
+        price: 5000,
+        productImage: product9,
+
+    },
+  
+
+
+];
